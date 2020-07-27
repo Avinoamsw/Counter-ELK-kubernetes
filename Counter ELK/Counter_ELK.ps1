fd5c44519d2e4c594ed3c56f1ea9c83057388ce9
Avinoam Swissa <avinoamsw@gmail.com>
2020-07-27 12:58:21 +0300

    Add wait for deployments

--- Counter ELK/Counter_ELK.ps1
+++ Counter ELK/Counter_ELK.ps1
@@ -2,14 +2,18 @@
 Start-Sleep -Seconds 5
 kubectl apply -f .\Helm-RBAC_ServiceAccount.yml
 kubectl apply -f .\Counter_Pod.yml
+kubectl wait --timeout=600s --for=condition=ready pod -l app=counter
 kubectl apply -f .\ElasticSearch_Service.yml
 kubectl apply -f .\ElasticSearch_PV.yml
 kubectl apply -f .\ElasticSearch_Deploy.yml
+kubectl wait --timeout=600s --for=condition=ready pod -l app=elasticsearch
 kubectl apply -f .\FileBeat_Service.yml
 kubectl apply -f .\FileBeat_ClusterRole.yml
 kubectl apply -f .\FileBeat_ClusterRoleBinding.yml
 kubectl apply -f .\FileBeat_Config.yml
 kubectl apply -f .\FileBeat_Deploy.yml
+kubectl wait --timeout=600s --for=condition=ready pod -l app=filebeat
 kubectl apply -f .\LogStash_Config.yml
 kubectl apply -f .\LogStash_Deploy.yml
+kubectl wait --timeout=600s --for=condition=ready pod -l app=logstash
 kubectl apply -f .\Kibana_Deploy.yml
